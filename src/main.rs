add5dc7342ef9b5ad72470cd18fc75587ba62019
mrxiaozhuox <mrxzx@wwsg18.top>
2022-01-27 23:18:56 +0800

    feat: commit code

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,52 @@
+#![allow(non_snake_case)]
+
+mod components;
+
+use std::process::exit;
+
+use components::*;
 use dioxus::prelude::*;
 
 fn main() {
-    // init debug tool for WebAssembly
-    wasm_logger::init(wasm_logger::Config::default());
-    console_error_panic_hook::set_once();
-
-    dioxus::web::launch(app); 
+    use dioxus::desktop::tao::dpi::LogicalSize;
+    dioxus::desktop::launch_cfg(app, |cfg| {
+        cfg.with_window(|w| {
+            w.with_title("Dioxus Lab | ⛺")
+                .with_resizable(true)
+                .with_inner_size(LogicalSize::new(1050.0, 650.0))
+        })
+    });
 }
 
 fn app(cx: Scope) -> Element {
 
+    let inner_path = use_state(&cx, || String::from("dashboard"));
+    
+    let body = match inner_path.get().as_str() {
+        "dashboard" => {
+            Dashboard(cx)
+        },
+        _ => None
+    };
+
     cx.render(rsx! (
-        div {
-            style: "text-align: center;",
-            h1 { "🌗 Dioxus 🚀" }
-            h3 { "Frontend that scales." }
-            p { "Dioxus is a portable, performant, and ergonomic framework for building cross-platform user interfaces in Rust." }
+        style { [ include_str!("./assets/bulma.min.css") ] }
+        // hidden the scroll bar
+        style { "html {{overflow-x: hidden; overflow-y: hidden;}}" }
+
+        navbar::Navbar {
+            path: inner_path
         }
+
+        body
+
+        script { [ include_str!("./assets/app.js") ] }
+    ))
+}
+
+
+fn Dashboard(cx: Scope) -> Element {
+    cx.render(rsx!(
+        h1 { "Dashboard" }
     ))
 }
